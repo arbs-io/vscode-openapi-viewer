04931a247e554250f255de78d3e74f18da7ac686
Andrew Butson <butsona@arbs.io>
2023-01-14 21:40:13 +0100

    fixed caseing

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import { ExtensionContext } from 'vscode'
-import { RegisterCommand } from './contexts/RegisterCommand'
+import { RegisterCommand } from './contexts/registerCommand'
 
 export function activate(context: ExtensionContext) {
   RegisterCommand(context)
