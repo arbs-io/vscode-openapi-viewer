a30dd335f7238bcb2c64874f78b3d1b74af2bd11
Andrew Butson <butsona@arbs.io>
2023-01-18 22:18:09 +0100

    refresh and title update

--- src/utils/activeEditorTracker.ts
+++ src/utils/activeEditorTracker.ts
@@ -1,54 +1,67 @@
-import * as vscode from 'vscode';
-import { Disposable } from './dispose';
-import { isSupportedLanguageMode } from './languageIds';
+import { TextEditor, EventEmitter, window } from 'vscode'
+import { Disposable } from './dispose'
+import { isSupportedLanguageMode } from './languageIds'
 
 export class ActiveEditorTracker extends Disposable {
+  private _activeEditor: TextEditor | undefined
 
-	private _activeEditor: vscode.TextEditor | undefined;
+  private readonly _onDidChangeActiveEditor = this._register(
+    new EventEmitter<TextEditor | undefined>()
+  )
+  public readonly onDidChangeActiveEditor = this._onDidChangeActiveEditor.event
 
-	private readonly _onDidChangeActiveEditor = this._register(new vscode.EventEmitter<vscode.TextEditor | undefined>());
-	public readonly onDidChangeActiveEditor = this._onDidChangeActiveEditor.event;
+  public constructor() {
+    super()
+    window.onDidChangeActiveTextEditor(
+      this._onDidChangeActiveTextEditor,
+      this,
+      this._disposables
+    )
+    window.onDidChangeVisibleTextEditors(
+      () => {
+        // Make sure the active editor is still in the visible set.
+        // This can happen if the output view is focused and the last active TS file is closed
+        if (this._activeEditor) {
+          if (
+            !window.visibleTextEditors.some(
+              (visibleEditor) => visibleEditor === this._activeEditor
+            )
+          ) {
+            this._onDidChangeActiveTextEditor(undefined)
+          }
+        }
+      },
+      this,
+      this._disposables
+    )
 
-	public constructor() {
-		super();
-		vscode.window.onDidChangeActiveTextEditor(this.onDidChangeActiveTextEditor, this, this._disposables);
-		vscode.window.onDidChangeVisibleTextEditors(() => {
-			// Make sure the active editor is still in the visible set.
-			// This can happen if the output view is focused and the last active TS file is closed
-			if (this._activeEditor) {
-				if (!vscode.window.visibleTextEditors.some(visibleEditor => visibleEditor === this._activeEditor)) {
-					this.onDidChangeActiveTextEditor(undefined);
-				}
-			}
-		}, this, this._disposables);
+    this._onDidChangeActiveTextEditor(window.activeTextEditor)
+  }
 
-		this.onDidChangeActiveTextEditor(vscode.window.activeTextEditor);
-	}
+  public get activeEditor(): TextEditor | undefined {
+    return this._activeEditor
+  }
 
-	public get activeEditor(): vscode.TextEditor | undefined {
-		return this._activeEditor;
-	}
+  private _onDidChangeActiveTextEditor(editor: TextEditor | undefined): any {
+    if (editor === this._activeEditor) {
+      return
+    }
 
-	private onDidChangeActiveTextEditor(editor: vscode.TextEditor | undefined): any {
-		if (editor === this._activeEditor) {
-			return;
-		}
+    if (editor && !editor.viewColumn) {
+      // viewColumn is undefined for the debug/output panel, but we still want
+      // to show the version info for the previous editor
+      return
+    }
 
-		if (editor && !editor.viewColumn) {
-			// viewColumn is undefined for the debug/output panel, but we still want
-			// to show the version info for the previous editor
-			return;
-		}
+    if (editor && this._isManagedFile(editor)) {
+      this._activeEditor = editor
+    } else {
+      this._activeEditor = undefined
+    }
+    this._onDidChangeActiveEditor.fire(this._activeEditor)
+  }
 
-		if (editor && this.isManagedFile(editor)) {
-			this._activeEditor = editor;
-		} else {
-			this._activeEditor = undefined;
-		}
-		this._onDidChangeActiveEditor.fire(this._activeEditor);
-	}
-
-	private isManagedFile(editor: vscode.TextEditor): boolean {
-		return isSupportedLanguageMode(editor.document);
-	}
+  private _isManagedFile(editor: TextEditor): boolean {
+    return isSupportedLanguageMode(editor.document)
+  }
 }
--- src/utils/languageIds.ts
+++ src/utils/languageIds.ts
@@ -1,8 +1,8 @@
-import * as vscode from 'vscode'
+import { TextDocument, languages } from 'vscode'
 
 export const json = 'json'
 export const openApiLanguageModes = [json]
 
-export function isSupportedLanguageMode(doc: vscode.TextDocument) {
-  return vscode.languages.match([json], doc) > 0
+export function isSupportedLanguageMode(doc: TextDocument) {
+  return languages.match([json], doc) > 0
 }
